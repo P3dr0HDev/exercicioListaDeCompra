35f5558627492a962f4d2e75e4eef346b0ac27a3
Pedro H. Ribeiro <pedrohribeiro.adv@gmail.com>
2025-11-14 13:57:29 -0300

    adicionado métodos principais. iniciado método checkout

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,125 @@ public class Main {
 
     }
 
-    static int lerInteiro(String prompt) {
+    static int optionMenu() {
+        System.out.println("""
+                +++++++++++++++++++++++++++++++++++++++++++++++++++++
+                                ADICIONE SUA MÚSICA
+                +++++++++++++++++++++++++++++++++++++++++++++++++++++
+                             1- Adicionar música
+                             2- Listar Músicas
+                             3- Buscar
+                             4- Filtrar por BPM
+                             5- Embaralhar Músicas
+                             6- Deletar Músicas
+                             0- sair ==========>
+                =====================================================
+                """);
+
+        int option = 0;
+        while (true) {
+            System.out.println("Opção: ");
+            if (!sc.hasNextInt()) {
+                System.out.println("Erro! Digite apenas números!");
+                sc.nextLine();
+                continue;
+
+            }
+            option = sc.nextInt();
+            sc.nextLine();
+            break;
+        }
+
+        return option;
+    }
+
+    static void menu() {
+        while(true) {
+            int option = optionMenu();
+
+            if (option == 0) {
+                System.out.println("Saindo do programa");
+                break;
+            }
+
+            switch (option) {
+                case 1 -> addItem();
+                case 2 -> deleteItem();
+                case 3 -> listItem();
+                case 4 -> checkout();
+
+                default -> {
+                    System.out.println("Opção inválida");
+                    menu();
+                }
+            }
+        }
+    }
+
+    static void addItem() {
+        System.out.println("Item: ");
+        String name = sc.nextLine();
+        System.out.println("Prioridade: ");
+        int priority = sc.nextInt();
+        System.out.println("Preço: ");
+        int price = sc.nextInt();
+        sc.nextLine();
+
+        Item item = new Item(name, priority, price);
+        itemList.add(item);
+
+        System.out.println("Item adicionad!");
+        menu();
+    }
+
+    static void listItem() {
+        System.out.println("+++++++Lista de Compra +++++++");
+
+        for (int i = 0; i < itemList.size(); i++) {
+            Item item = itemList.get(i);
+            System.out.printf("""
+                    +++++++++++++++++++
+                    Quantidade de itens: %d
+                    Nome: %s
+                    Prioridade: %d
+                    Preço: %d
+                    +++++++++++++++++++
+                    """, itemList.size(), item.getName(), item.getPriority(), item.getPrice());
+        }
+        menu();
+    }
+
+    static void deleteItem() {
+        System.out.println("Remover produto\n");
+        listItem();
+
+        while (true) {
+            System.out.println("Digite o número do produto");
+            if (!sc.hasNextInt()) {
+                System.out.println("Erro, digite apenas números!\n");
+                continue;
+            }
+
+            int index = sc.nextInt();
+            sc.nextLine();
+
+            if (index >= 0 && index < Main.itemList.size()) {
+                System.out.println("Produto removido!");
+
+                break;
+            } else {
+                System.out.println("ERRO. Produto não existe");
+            }
+        }
+        menu();
+
+    }
+
+    static void checkout(){
+
+    }
+
+    /*static int lerInteiro(String prompt) {
         while (true) {
             System.out.print(prompt);
 
@@ -25,6 +143,6 @@ public class Main {
 
             return numero;
         }
-    }
+    }*/
 
 }
