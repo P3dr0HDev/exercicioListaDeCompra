2c108f1527407436f1cccc00bc53df76115572d2
Pedro H. Ribeiro <pedrohribeiro@gmail.com>
2025-11-13 21:33:02 -0300

    mais código

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -1,2 +1,25 @@
 public class Item {
+    private String name;
+    private int priority;
+
+    public Item (String name, int priority){
+        this.name = name;
+        this.priority = priority;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+    static Scanner sc = new Scanner(System.in);
+    static List<Item> itemList = new ArrayList<>();
+
     public static void main(String[] args) {
 
     }
+
+    static int lerInteiro(String prompt) {
+        while (true) {
+            System.out.print(prompt);
+
+            if (!sc.hasNextInt()) {
+                System.out.println("*** Erro, digite apenas numeros ***");
+                sc.nextLine();
+                continue;
+            }
+
+            int numero = sc.nextInt();
+            sc.nextLine();
+
+            return numero;
+        }
+    }
+
 }
